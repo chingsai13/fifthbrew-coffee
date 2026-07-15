20c2e56950f125fa215623aff297717bed11b073
Angelo Ching Sai <angelochingsai@Angelos-MacBook-Pro.local>
2026-07-15 17:11:38 +0800

    Add live DB credentials

--- config/db.php
+++ config/db.php
@@ -3,10 +3,10 @@
 // DATABASE CONNECTION - EDIT THESE 4 VALUES AFTER YOU CREATE
 // YOUR DATABASE ON YOUR FREE HOST (InfinityFree / AwardSpace).
 // ============================================================
-$db_host = "localhost";      // e.g. sqlXXX.infinityfree.com
-$db_user = "your_db_user";   // from your host's MySQL panel
-$db_pass = "your_db_pass";   // from your host's MySQL panel
-$db_name = "your_db_name";   // from your host's MySQL panel
+$db_host = "sql102.infinityfree.com";      // e.g. sqlXXX.infinityfree.com
+$db_user = "if0_42415542";   // from your host's MySQL panel
+$db_pass = "Wishbone13333";   // from your host's MySQL panel
+$db_name = "if0_42415542_fifthbrewdb";   // from your host's MySQL panel
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
